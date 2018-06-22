af7a72598b47f0a14af1aaf5490caabd252a1a48
Steven Speek <slspeek@gmail.com>
2018-06-22 20:40:58 +0200

    Paradise radio added

--- das/do-scripts.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -e
-
-
-TMPFILE=$(mktemp)
-mkdir -p /tmp/fai
-touch /tmp/fai/status.log
-
-fai-class /srv/fai/config/class $TMPFILE
-FAI=/srv/fai/config classes=$(cat $TMPFILE) FAI_ROOT=/ fai-do-scripts /srv/fai/config/scripts
---
+++ das/paradise.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+mpg123 -@ http://stream-uk1.radioparadise.com/mp3-128
